5b9e4a15cddbb0fd93a15b6e3e1dfe409da93cfb
Ali Eissa <137681197+10-ALIen-01@users.noreply.github.com>
2024-09-25 00:57:47 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,6 +14,17 @@ window.onscroll = function () {
     }
 };
 
+window.onscroll = function () {
+    var header = document.querySelector('header');
+    var sticky = header.offsetTop;
+    
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+};
+
 
 // Hamburger Menu Toggle
 document.getElementById('menu-toggle').addEventListener('click', function () {
